122c9647e5755a48f969c761ae712657ec9d2936
Joseph <joseph@larmarange.net>
2013-08-17 00:59:32 +0200

    R CMD build for the tar.gz

--- CheckAndBuildPrevR.bat
+++ CheckAndBuildPrevR.bat
@@ -1,7 +1,7 @@
 rmdir prevR.Rcheck /S /Q
 R CMD check prevR 
 R CMD build prevR --compact-vignettes
-REM attention remplacer si nécessaire le numero de version dans le nom du fichier tar.gz
-R CMD INSTALL --build  prevR_2.3.tar.gz
+R CMD build prevR
 R CMD INSTALL --build  prevR
-R CMD check --as-cran prevR_2.3.tar.gz
+
+REM R CMD check --as-cran prevR_2.3.tar.gz
