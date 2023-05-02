6d8a7fac827d159d15ab5c9f23642ac1e0f6c388
Joseph Larmarange <joseph@larmarange.net>
2023-05-02 16:32:27 +0200

    theme update

--- R/theme_prevR.r
+++ R/theme_prevR.r
@@ -1,19 +1,20 @@
 #' prevR themes for ggplot2
-#' 
+#'
 #' Two custom themes for ggplot2 graphs, hiding axis.
-#' 
+#'
 #' @param base_size base font size
-#' @importFrom ggplot2 '%+replace%'
-#' 
+#' @importFrom ggplot2 '%+replace%' theme theme_grey element_blank unit
+#'
 #' @export
 
-theme_prevR <- function (base_size = 12) {
-  ggplot2::'%+replace%'(theme_grey(base_size),
-    theme(
-      axis.title        = element_blank(),
-      axis.text         = element_blank(),
-      axis.ticks.length = unit(0, "cm"),
-      plot.margin       = unit(c(0, 0, 0, 0), "lines"),
+theme_prevR <- function(base_size = 12) {
+  ggplot2::"%+replace%"(
+    ggplot2::theme_grey(base_size),
+    ggplot2::theme(
+      axis.title        = ggplot2::element_blank(),
+      axis.text         = ggplot2::element_blank(),
+      axis.ticks.length = ggplot2::unit(0, "cm"),
+      plot.margin       = ggplot2::unit(c(0, 0, 0, 0), "lines"),
       complete          = TRUE
     )
   )
@@ -21,16 +22,16 @@ theme_prevR <- function (base_size = 12) {
 
 #' @export
 #' @rdname theme_prevR
-
-theme_prevR_light <- function (base_size = 12) {
-  '%+replace%'(theme_grey(base_size),
-    theme(
-      axis.title        = element_blank(),
-      axis.text         = element_blank(),
-      panel.background  = element_blank(),
-      panel.grid        = element_blank(),
-      axis.ticks.length = unit(0, "cm"),
-      plot.margin       = unit(c(0, 0, 0, 0), "lines"),
+theme_prevR_light <- function(base_size = 12) {
+  ggplot2::"%+replace%"(
+    ggplot2::theme_grey(base_size),
+    ggplot2::theme(
+      axis.title        = ggplot2::element_blank(),
+      axis.text         = ggplot2::element_blank(),
+      panel.background  = ggplot2::element_blank(),
+      panel.grid        = ggplot2::element_blank(),
+      axis.ticks.length = ggplot2::unit(0, "cm"),
+      plot.margin       = ggplot2::unit(c(0, 0, 0, 0), "lines"),
       complete          = TRUE
     )
   )
