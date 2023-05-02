360919719d2a350d96e21bfe84d104695e779dca
Joseph Larmarange <joseph@larmarange.net>
2023-05-02 16:30:23 +0200

    print and show

--- R/print.prevR.r
+++ R/print.prevR.r
@@ -1,13 +1,13 @@
 #' Summary of a prevR object.
-#' 
-#' Method \code{print} for objects of class [prevR-class]: 
+#'
+#' Method \code{print} for objects of class [prevR-class]:
 #' shows a summary of the object's characteristics.
-#' 
+#'
 #' @param x object of class [prevR-class].
-#' 
+#'
 #' @note Exactly the same as [show()].
 #' @seealso [summary()].
-#' @examples 
+#' @examples
 #' print(fdhs)
 #' \dontrun{
 #'  dhs <- rings(fdhs,N=c(100,300,500))
@@ -17,9 +17,9 @@
 #' @aliases print print-methods print,prevR-method
 #' @exportMethod print
 
-setMethod("print","prevR",
-function(x){
-   show(x)
-   invisible(NULL)
-  }
+setMethod("print", "prevR",
+  function(x) {
+     show(x)
+     invisible(NULL)
+    }
 )
