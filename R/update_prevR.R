3b988984a44a8a577a34aeae973aca3d1ba6b6fb
Joseph Larmarange <joseph@larmarange.net>
2023-05-15 11:36:44 +0200

    lint

--- R/update_prevR.R
+++ R/update_prevR.R
@@ -1,30 +1,30 @@
 #' Update a prevR object
-#' 
+#'
 #' Update an object of class `prevR` created with a previous version of the
 #' package to the last version. In particular, it will convert any boundary
 #' slot defined with the `sp` package to `sf` class.
-#' 
+#'
 #' @param object a `prevR` object
 #' @return a `prevR` object
 #' @export
 update_prevR <- function(object) {
   if (!inherits(object, "prevR"))
     stop("'object' should be of class 'prevR'.")
-  
+
   if (attr(object@boundary, "valid"))
     boundary <- as(object@boundary, "sf")
   else
     boundary <- NULL
   col <- colnames(object@clusters)
   names(col) <- col
-  
+
   res <- as.prevR(
     data = object@clusters,
     col = col,
     boundary = boundary,
     proj = st_crs(object@proj)
   )
-  
+
   N <- NULL
   R <- NULL
   # On verifie si rings est vide
@@ -37,6 +37,6 @@ update_prevR <- function(object) {
   if (!is.null(N) && !is.null(R)) {
     res <- rings(res, N = N, R = R)
   }
-  
+
   res
 }
